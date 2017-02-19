4dcaf29f6f1c55689690c30f725a1ae35ae28687
meeshyd <michellemdidier@gmail.com>
2017-02-19 12:31:17 -0600

    commiting change to game.js- added if statement to checkLetters function to check if UserInput is a letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ var wordBank = [
 	'memow'
 ];
 
-var wordChoice = "";
+var wordChoice = '';
 var wordChoiceLetters = [];
 var numberOfBlanks = 0;
 var blanksWins = [];
@@ -28,46 +28,53 @@ function start() {
 	//pick word from word bank
 	wordChoice = wordBank[Math.floor(Math.random() * wordBank.length)];
 	//split word into letters
-	wordChoiceLetters = wordChoice.split("");
+	wordChoiceLetters = wordChoice.split('');
 	//determine # blanks for each letter in random word by finding length of lettersInSelected
 	numberOfBlanks = wordChoiceLetters.length;
 
 	for( var i = 0; i < numberOfBlanks; i++) {
-		blanksWins.push("_");
+		blanksWins.push('_');
 	}
 	document.getElementById('blanks').innerHTML=blanksWins.join(" ");
 	document.getElementById('guesses-left').innerHTML=guessesLeft;
 }
 
 function checkLetters(letters) {
+//check is user input is a letter
+//if it is, proceed to check letter against computer word. 
+//if not, alert 'letters only'
+	var goodLetters= /^[A-Za-z]+$/;
+	if (letters.match(goodLetters)) {
 
-	// if correct change blanks to user selections
-	
 	// compare user selections against chosen word
-	var letterChoice = false;
-
-	for (var i = 0; i<numberOfBlanks; i++) {
-		if (letters===wordChoice[i]){
-			letterChoice = true;
-		}
-	}
-	// if user letter matches, then the above loop assigns true to if 
-	// the above loop is true run this
-	if (letterChoice) {
-		for (i = 0; i<numberOfBlanks; i++) {
+		// var letterChoice = false;
+		var letterChoice = false;
+		for (var i = 0; i<numberOfBlanks; i++) {
 			if (letters===wordChoice[i]){
-			blanksWins[i] = letters;	
-		}	
-	// if incorrect guessed letter goes to wrong letters array
-	// if incorrect also subtract 1 from guessesLeft
+				letterChoice = true;
+			}
 		}
-	}else {
-		if(wrongLetters.indexOf(letters)> -1){
-              } else{
-          wrongLetters.push(letters);
-          guessesLeft--;
-		}	
+		// if user letter matches, then the above loop assigns true to if 
+		// the above loop is true run this
+		if (letterChoice) {
+			for (i = 0; i<numberOfBlanks; i++) {
+				if (letters===wordChoice[i]){
+				blanksWins[i] = letters;	
+			}	
+		// if incorrect guessed letter goes to wrong letters array
+		// if incorrect also subtract 1 from guessesLeft
+		}
+		}else {
+			if(wrongLetters.indexOf(letters)> -1){
+	              } else{
+	          wrongLetters.push(letters);
+	          guessesLeft--;
+			}	
+		}
+	} else {
+		alert ('Letters only!');
 	}
+
 }
 
 
