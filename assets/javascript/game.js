306976b6ea112cfc3dbb938763ef673eba05015a
meeshyd <michellemdidier@gmail.com>
2017-02-18 21:34:41 -0600

    commiting changes to game.js - added functions to compare user input to chosen word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-// array with list of possible words
 var wordBank = [
 	'gunter',
 	'finn',
@@ -10,22 +9,10 @@ var wordBank = [
 	'memow',
 ]
 
-// someway to isolate one word for user guesses
-//identify chosen word with a randomizing function
 var wordChoice = "";
-
-//array with letters in selectedWord
-var lettersInSelected = [];
-
-//# of letters in SelectedWord
+var wordChoiceLetters = [];
 var numberOfBlanks = 0;
 var blanksWins = [];
-
-// onkey function for user letter guess
-// store user letter guess and compare to computer word
-// 	guessed letters
-var correctLetters = [];
-// 	wrong letters
 var wrongLetters = [];
 // 	correct letters??
 //  repeat letters??
@@ -34,44 +21,78 @@ var wrongLetters = [];
 // show blanks for word pick and change to letters in word pick as user guesses letters
 
 //score keeping
-var winCount=0;
-var lossCount=0;
-var guessesLeft=10;
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 10;
 
 
 function start() {
+	wrongLetters = [];
+	guessesLeft = 10;
+	blanksWins = [];
 	//pick word from word bank
 	wordChoice = wordBank[Math.floor(Math.random() * wordBank.length)];
 	//split word into letters
-	lettersInSelected = wordChoice.split("");
+	wordChoiceLetters = wordChoice.split("");
 	//determine # blanks for each letter in random word by finding length of lettersInSelected
-	numberOfBlanks = lettersInSelected.length;
-	//for loop runs if i is less than # of blanks
+	numberOfBlanks = wordChoiceLetters.length;
 	console.log (wordChoice);
 	console.log (numberOfBlanks);
 
 	for( var i = 0; i < numberOfBlanks; i++) {
 		blanksWins.push("_");
 	}
-	console.log (blanksWins);
+	console.log(blanksWins);
+	document.getElementById('blanks').innerHTML=blanksWins.join(" ");
+	document.getElementById('guesses').innerHTML=guessesLeft;
 }
+/*runs start function*/
+start();
 
 function checkLetters(letters) {
-	// compare user selections against chosen word
+
 	// if correct change blanks to user selections
+	
+	// compare user selections against chosen word
+	var letterChoice = false;
+	for (i=0; i<numberOfBlanks; i++) {
+		if (letters===wordChoice[i]){
+			letterChoice = true;
+		}
+	}
+	// if user letter matches, then the above loop assigns true to if the above loop is true run this
+	if (letterChoice) {
+		for (i=0; i<numberOfBlanks; i++) {
+			if (letters===wordChoice[i]){
+			blanksWins[i] = letters;
+		}	
+	}
+	console.log('inside check letter ', blanksWins);
 	// if incorrect guessed letter goes to wrong letters array
 	// if incorrect also subtract 1 from guessesLeft
+	} else {
+		guessesLeft --;
+		wrongLetters.push(letters);
+	}
+	console.log('wrong guess in check letter', wrongLetters);
 }
 
-function updateHTML() {
+function addHTML() {
 	//letters that are in word
 	//guesses left
 	//wrongly guessed letters
 	//determine win or lose
 }
-start();
 
-document.onkeyup = function(event) {
+document.onkeyup = function() {
 	//takes user input
 	//runs through checkLetters
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log("typed letter: "+ userGuess);
+	checkLetters(userGuess);
+
+}
+
+function restart() {
+	//call start function when player wins or loses
 }
